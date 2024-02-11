260c1b30c3af95b2dbbc948b5ee1f465d7a0781e
vincenttang1101 <tangtrinhquang@gmail.com>
2024-02-11 18:21:45 +0700

    refactor: config tailwind #2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const plugin = require('tailwindcss/plugin')
-/** @type {import('tailwindcss').Config} */
 
+/** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  corePlugins: {
+    container: false
+  },
   theme: {
     extend: {
       colors: {
