0f1ecc2a6d65a99650e841d66973ffef3ebca642
vincenttang1101 <tangtrinhquang@gmail.com>
2024-02-18 22:29:20 +0700

    refactor: organize react query #1

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import { Login, ProductList, Register } from '@/pages'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import App from '@/App'
 import ReactDOM from 'react-dom/client'
@@ -43,8 +45,19 @@ const router = createBrowserRouter([
   }
 ])
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  }
+})
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 )
